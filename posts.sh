db9de41b7cf6c9f160c4d704c701686173a7741c
Mango0x45 <thomasvoss@live.com>
2021-07-17 00:34:05 +0200

    Genesis commit

---
+++ posts.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+:>counts
+while IFS= read -r USER; do
+	2>/dev/null curl "https://www.speedrun.com/user/$USER/info" |
+		sed -n 's|.*<p><strong>Posts:</strong> \([0-9]*\)</p>.*|\1 '$USER'|p' >>counts
+done <users
+
+sort -nr counts | awk '
+{
+	if ($1 == prev)
+		line = prev_nr
+	else
+		line = NR
+
+	print "<tr><td>"line"</td><td>"$2"</td><td>"$1"</td></tr>"
+	prev = $1
+	prev_nr = line
+}
+' >posts
+
+sed "
+s/<!-- LAST UPDATED -->/$(date "+%d %B %Y, %T")/
+/<!-- POST DATA -->/r posts
+" template.html >index.html
